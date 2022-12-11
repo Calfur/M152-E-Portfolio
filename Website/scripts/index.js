acfea515bec80ea9f0890385cc0b17f4e6532b04
Calfur <robin.nater@hotmail.com>
2022-12-11 19:52:26 +0100

    implement color slider with css variable

--- Website/scripts/index.js
+++ Website/scripts/index.js
@@ -1,3 +1,11 @@
+var dynamicColor1 = [0, 149, 243];
+var dynamicColor2 = [242, 34, 83];
+
+$("#color-slider").on("input", function () {
+   var rgb = getGradiantValue($(this).val()/100);
+   $("body").get(0).style.setProperty("--dynamic-color", `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`);
+});
+
 function scrollToId(target) {
    $('body,html').animate({
       scrollTop: - 50
@@ -13,4 +21,14 @@ $('#back-to-top').click(function () {
 
 function scrollToDiv(id) {
    $('html,body').unbind().animate({ scrollTop: $(id).offset().top - 59 }, 0);
+};
+
+function getGradiantValue(shareColor1) {
+   var shareColor2 = 1 - shareColor1;
+
+   var r = parseInt(dynamicColor1[0] * shareColor1 + dynamicColor2[0] * shareColor2);
+   var g = parseInt(dynamicColor1[1] * shareColor1 + dynamicColor2[1] * shareColor2);
+   var b = parseInt(dynamicColor1[2] * shareColor1 + dynamicColor2[2] * shareColor2);
+
+   return [r, g, b];
 };
